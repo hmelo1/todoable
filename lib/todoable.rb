a63842e08bb5f83da9cc60772e228ee880900cb2
Heriberto Melo <heriberto@melo.nyc>
2019-02-07 19:05:38 -0500

    Added constant paths

--- lib/todoable.rb
+++ lib/todoable.rb
@@ -2,5 +2,15 @@ require "todoable/list"
 require "todoable/item"
 
 module Todoable
+    BASE_ROUTE = 'http://todoable.teachable.tech/api'
+    AUTH_PATH = "/authenticate"
+    LISTS_PATH = "/lists" [GET, POST]
+    LIST_PATH = "/lists/:list_id" [GET, PATCH, DELETE]
+    ITEMS_PATH = "/lists/:list_id/items" [POST]
+    ITEM_PATH = "/lists/:list_id/items/:item_id" [DELETE]
+    FINISHED_ITEM_PATH = "/lists/:list_id/items/:item_id/finish" [DELETE]
+
+
+
 
 end
--- spec/todoable_spec.rb
+++ spec/todoable_spec.rb
@@ -1,11 +1,5 @@
 require 'todoable'
 
 RSpec.describe Todoable do
-    it "Has a List class" do
-        expect(Todoable::List.portray("Broccoli")).to eql("Gross!")
-    end
 
-    it "Has a Item class" do
-        expect(Todoable::Item.portray("Broccoli")).to eql("Gross!")
-    end
 end
